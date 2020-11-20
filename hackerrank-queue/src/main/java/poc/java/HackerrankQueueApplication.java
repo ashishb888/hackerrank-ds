52b9d94e6442caa099283cfd86df3f3b20529be9
ashishb888 <ashish.bhosle008@gmail.com>
2020-11-20 15:07:45 +0530

    hackerrank-queue: Initial commit

---
+++ hackerrank-queue/src/main/java/poc/java/HackerrankQueueApplication.java
@@ -0,0 +1,13 @@
+package poc.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerrankQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackerrankQueueApplication.class, args);
+	}
+
+}
