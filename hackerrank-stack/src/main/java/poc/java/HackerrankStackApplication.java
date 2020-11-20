ee3cef102ca74cc0ab007dcb55d5ef968c8b9042
ashishb888 <ashish.bhosle008@gmail.com>
2020-11-20 15:56:34 +0530

    hackerrank-stack: Initial commit

---
+++ hackerrank-stack/src/main/java/poc/java/HackerrankStackApplication.java
@@ -0,0 +1,13 @@
+package poc.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerrankStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackerrankStackApplication.class, args);
+	}
+
+}
